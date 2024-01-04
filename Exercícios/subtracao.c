69b743da1fe7b82de5ba5af5441c25f18a839b29
Matheus Maciel <134061655+MTFalcao@users.noreply.github.com>
2024-01-04 13:08:18 -0300

    Create subtracao.c

---
+++ Exercícios/subtracao.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+	int a, b, sub;
+	
+	printf(" Insira os numeros para realizar uma subtracao");
+    
+   printf("\n Insira o primeiro numero: ");
+   scanf("%d", &a);
+   
+   printf("Insira o segundo numero: ");
+   scanf("%d", &b);
+	
+	sub = a- b;
+	printf("\n O resultado da subtracao realizada foi: %d", sub);
+	}
